95f3d20b1bb031db77918203efe4a2ec6af8277d
Nikolay Ponomarev <n.s.ponomarev@yandex.ru>
2023-02-27 16:32:15 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+/*! For license information please see main.js.LICENSE.txt */
+!function(){"use strict";var e={r:function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}};e.r({}),console.log("JS is Work")}();
+//# sourceMappingURL=main.js.map
